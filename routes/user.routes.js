4f828302b041189ae1597fcf997242657cf416da
anggriawanmr <anggriawanmr@gmail.com>
2023-02-14 13:10:25 +0700

    Creating controllers and routes in server side :sparkles:

---
+++ routes/user.routes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+// Import all the controllers...
+import {
+  createUser,
+  getAllUsers,
+  getUserInfoById,
+} from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.route('/').get(getAllUsers);
+router.route('/').post(createUser);
+router.route('/:id').get(getUserInfoById);
+
+export default router;
